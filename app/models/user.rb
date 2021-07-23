982155582c6d640098af2f670397f1eba340f18d
danielbossier <dbossier21@gmail.com>
2021-07-23 13:20:38 -0700

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
-
+  has_many :teams
+  has_many :leagues, through: :teams
+  belongs_to :groups
 end
