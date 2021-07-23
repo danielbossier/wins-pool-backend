d176b0656415051cfbf6ae463248e57d1ee37886
danielbossier <dbossier21@gmail.com>
2021-07-23 13:39:58 -0700

    added group dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# group = Group.create!(name: "HOFers")
+
+user = User.create!(username: "Babe Ruth", email: "babe@example.com", group_id: 1)
+
+# league = League.create!(name: "MLB")
+
+# team = Team.create!(name: "Yankees", wins: 10, losses: 8, ties: 0, league_id: 1, user_id: 1)
